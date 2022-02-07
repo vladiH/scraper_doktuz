096584a64173d69531aaa13cc13a61aea39c2735
Yuri V. Huallpa Vargas <yurihuallpavargas@gmail.com>
2022-02-07 17:13:04 -0500

    .

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__": # EJECUCION POR INTERFAZ GRAFICA
     Logger.warning("App started")
     scrapper_doktuz('01-01-2022', '27-01-2022')
     logging.shutdown()
-# EJECUCION POR TERMINAL
+# EJECUCION POR TERMINAL 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
 # scrapy runspider ./src/domain/usecases/get_data.py -o doktuz.csv -t csv 
