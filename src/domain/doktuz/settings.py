4f562e0cc6b4060330b8731d38d1643c86f03424
Yuri V. Huallpa Vargas <yurihuallpavargas@gmail.com>
2022-02-22 16:49:25 -0500

    .

--- src/domain/doktuz/settings.py
+++ src/domain/doktuz/settings.py
@@ -68,7 +68,7 @@ ITEM_PIPELINES = {
     'src.domain.doktuz.selenium_pipeline.DoktuzSeleniumPipeline': 400,
     #'src.domain.doktuz.data_base_pipeline.DatabasePipeline': 500,
     'src.domain.doktuz.excel_file_pipeline.ExcelFilePipeline': 1,
-    'src.domain.doktuz.save_excel_pipeline.SaveExcelPipeline': 2,
+    #'src.domain.doktuz.save_excel_pipeline.SaveExcelPipeline': 2,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
